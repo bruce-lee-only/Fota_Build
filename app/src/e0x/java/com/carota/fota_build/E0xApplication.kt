614e01473c7c6e209455f3a17c0685548e65e6cb
bruce-lee-only <154116604+bruce-lee-only@users.noreply.github.com>
2023-12-28 09:51:11 +0800

    增加对于APP版本信息的打印功能

--- app/src/e0x/java/com/carota/fota_build/E0xApplication.kt
+++ app/src/e0x/java/com/carota/fota_build/E0xApplication.kt
@@ -1,4 +1,14 @@
 package com.carota.fota_build
 
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
 class E0xApplication: BaseApplication() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun minorVersion(): String {
+        return ".01"
+    }
 }
--- app/src/fat/java/com/carota/fota_build/BaseApplication.kt
+++ app/src/fat/java/com/carota/fota_build/BaseApplication.kt
@@ -1,14 +1,19 @@
 package com.carota.fota_build
 
 import android.app.Application
-import com.carota.util.LogUtil
+import com.carota.lib.common.uitls.Logger
 
-open class BaseApplication: Application() {
+abstract class BaseApplication: Application(), IApplication{
 
     override fun onCreate() {
         super.onCreate()
 
         //todo:初始化-log系统
-        LogUtil.initLogger(applicationContext)
+        Logger.initLogger(this)
+
+        //todo: 打印当前配置版本信息+小版本信息
+        Logger.info("APP VERSION: ${BuildConfig.EXIT_VERSION_CODE}${minorVersion()}")
     }
+
+    abstract override fun minorVersion(): String
 }
